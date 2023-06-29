d9610e7a23358f51c0c3071221605e860582f26a
Pedro Lima <tsc.pedrolima@gmail.com>
2023-06-28 22:09:26 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss'
+
+function App() {
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
