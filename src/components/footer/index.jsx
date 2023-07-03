e71f7bbec24213dc7e4d8ee8c38b814919399cf3
Pedro Lima <tsc.pedrolima@gmail.com>
2023-07-03 10:11:45 -0300

    feat: add navigate function at the footer component

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,12 +1,22 @@
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import './index.scss'
 
 function Footer() {
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        navigate(-1)
+    }
+
+    const goForward = () => {
+        navigate(1)
+    }
+
     return (
         <div className='row py-3 px-5 justify-content-around position-custom'>
             <div className="col-12 d-flex justify-content-between col-md-10">
-                <Link className=''>Go back</Link>
-                <Link className=''>Next Step</Link>
+                <button onClick={goBack} className=''>Go back</button>
+                <button onClick={goForward} className=''>Next Step</button>
             </div>
         </div>
     )
