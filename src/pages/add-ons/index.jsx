1539ece19188d87cd72b1f3d6ee86ea670e39554
Pedro Lima <tsc.pedrolima@gmail.com>
2023-07-07 17:57:26 -0300

    feat: add state to add-ons page inputs

--- src/pages/add-ons/index.jsx
+++ src/pages/add-ons/index.jsx
@@ -1,19 +1,31 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom'
+
 import './index.scss'
 
 import { BsCheckSquareFill } from 'react-icons/bs';
 
 function AddOns() {
-
     const navigate = useNavigate();
 
     const goBack = () => {
         navigate(-1)
     }
 
-    const goForward = () => {
-        navigate("/summary")
+    const [addOns, setAddOns] = useState(null)
+    const handleChange = ({ target }) => {
+        const { id, value } = target;
+        setAddOns(prev => ({ ...prev, [id]: value }))
     }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (addOns) {
+            navigate("/summary")
+        }
+    }
+
+    console.log(addOns)
 
     return (
         <section className="bg-white d-flex flex-column justify-content-between" id="add-ons">
@@ -21,8 +33,14 @@ function AddOns() {
                 <h3 className='font-tertiary-color'>Pick add-ons</h3>
                 <p className='font-primary-color'>Add-ons help enhance your gaming experience.</p>
             </div>
-            <form className='pt-md-2 justify-content-md-between'>
-                <input name='plan' id='service' type="checkbox" className='d-none' />
+            <form onSubmit={handleSubmit} className='pt-md-2 justify-content-md-between'>
+                <input
+                    id='service'
+                    type="checkbox"
+                    className='d-none'
+                    value="Online service"
+                    onChange={handleChange}
+                />
                 <label htmlFor="service" className="card flex-row align-items-center p-3 border-custom text-left">
                     <BsCheckSquareFill />
                     <div className="card-body p-0 ml-3 ml-md-4">
@@ -31,7 +49,13 @@ function AddOns() {
                     </div>
                     <span className='font-color-custom'>+$1/mo</span>
                 </label>
-                <input name='plan' id='storage' type="checkbox" className='d-none' />
+                <input
+                    id='storage'
+                    type="checkbox"
+                    className='d-none'
+                    value="Larger storage"
+                    onChange={handleChange}
+                />
                 <label htmlFor="storage" className="card flex-row align-items-center p-3 border-custom flex-md-row text-left">
                     <BsCheckSquareFill />
                     <div className="card-body p-0 ml-3 ml-md-4">
@@ -40,20 +64,26 @@ function AddOns() {
                     </div>
                     <span className='font-color-custom'>+$2/mo</span>
                 </label>
-                <input name='plan' id='profile' type="checkbox" className='d-none' />
+                <input
+                    id='profile'
+                    type="checkbox"
+                    className='d-none'
+                    value="Customizable profile"
+                    onChange={handleChange}
+                />
                 <label htmlFor="profile" className="card flex-row align-items-center p-3 border-custom flex-md-row text-left">
                     <BsCheckSquareFill />
                     <div className="card-body p-0 ml-3 ml-md-4">
-                        <h6 className="card-title m-0 font-tertiary-color">Customizable Profile</h6>
+                        <h6 className="card-title m-0 font-tertiary-color">Customizable profile</h6>
                         <p className="card-text font-primary-color font-size-custom">Custom theme on your profile</p>
                     </div>
                     <span className='font-color-custom'>+$2/mo</span>
                 </label>
+                <div className="d-flex justify-content-between p-3 px-md-0 position-custom">
+                    <button onClick={goBack} className='border-0 bg-white font-primary-color weight-custom'>Go Back</button>
+                    <button type='submit' className='border-0 rounded p-2 px-md-4 bg-button-color text-white weight-custom'>Next Step</button>
+                </div>
             </form>
-            <div className="d-flex justify-content-between p-3 px-md-0 position-custom">
-                <button onClick={goBack} className='border-0 bg-white font-primary-color weight-custom'>Go Back</button>
-                <button onClick={goForward} type='submit' className='border-0 rounded p-2 px-md-4 bg-button-color text-white weight-custom'>Next Step</button>
-            </div>
         </section>
     )
 }
