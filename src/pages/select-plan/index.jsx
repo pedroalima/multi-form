274d40fd56a6f28ab8165d68c8ad892eb629c09f
Pedro Lima <85854788+pedroalima@users.noreply.github.com>
2023-07-08 17:03:43 -0300

    feat: add validation to your-info page input

--- src/pages/select-plan/index.jsx
+++ src/pages/select-plan/index.jsx
@@ -32,52 +32,54 @@ function SelectPlan() {
                 <h3 className='font-tertiary-color'>Select your plan</h3>
                 <p className='font-primary-color'>You have the option of monthly or yearly billing.</p>
             </div>
-            <form onSubmit={handleSubmit} className='d-md-flex justify-content-md-between'>
-                <input
-                    name='plan'
-                    id='arcade'
-                    type="radio"
-                    className='d-none'
-                    value="arcade"
-                    onChange={(e) => setPlan(e.target.value)}
-                />
-                <label htmlFor="arcade" className="card flex-row p-3 border-custom flex-md-column text-left width-custom">
-                    <img src={iconArcade} className='mb-md-5' width='42' alt="Icon arcade" />
-                    <div className="card-body p-0 ml-3 ml-md-0">
-                        <h6 className="card-title m-0 font-tertiary-color">Arcade</h6>
-                        <p className="card-text font-primary-color">$9/mo</p>
-                    </div>
-                </label>
-                <input
-                    name='plan'
-                    id='advanced'
-                    type="radio"
-                    className='d-none'
-                    value="advanced"
-                    onChange={(e) => setPlan(e.target.value)}
-                />
-                <label htmlFor="advanced" className="card flex-row p-3 border-custom flex-md-column text-left width-custom">
-                    <img src={iconAdvanced} className='mb-md-5' width='42' alt="Icon advanced" />
-                    <div className="card-body p-0 ml-3 ml-md-0">
-                        <h6 className="card-title m-0 font-tertiary-color">Advanced</h6>
-                        <p className="card-text font-primary-color">$12/mo</p>
-                    </div>
-                </label>
-                <input
-                    name='plan'
-                    id='pro'
-                    type="radio"
-                    className='d-none'
-                    value="pro"
-                    onChange={(e) => setPlan(e.target.value)}
-                />
-                <label htmlFor="pro" className="card flex-row p-3 border-custom flex-md-column text-left width-custom">
-                    <img src={iconPro} className='mb-md-5' width='42' alt="Icon pro" />
-                    <div className="card-body p-0 ml-3 ml-md-0">
-                        <h6 className="card-title m-0 font-tertiary-color">Pro</h6>
-                        <p className="card-text font-primary-color">$14/mo</p>
-                    </div>
-                </label>
+            <form onSubmit={handleSubmit} className='d-md-flex flex-md-column justify-content-md-between'>
+                <div className='d-md-flex justify-content-md-between'>
+                    <input
+                        name='plan'
+                        id='arcade'
+                        type="radio"
+                        className='d-none'
+                        value="arcade"
+                        onChange={(e) => setPlan(e.target.value)}
+                    />
+                    <label htmlFor="arcade" className="card flex-row p-3 border-custom flex-md-column text-left width-custom">
+                        <img src={iconArcade} className='mb-md-5' width='42' alt="Icon arcade" />
+                        <div className="card-body p-0 ml-3 ml-md-0">
+                            <h6 className="card-title m-0 font-tertiary-color">Arcade</h6>
+                            <p className="card-text font-primary-color">$9/mo</p>
+                        </div>
+                    </label>
+                    <input
+                        name='plan'
+                        id='advanced'
+                        type="radio"
+                        className='d-none'
+                        value="advanced"
+                        onChange={(e) => setPlan(e.target.value)}
+                    />
+                    <label htmlFor="advanced" className="card flex-row p-3 border-custom flex-md-column text-left width-custom">
+                        <img src={iconAdvanced} className='mb-md-5' width='42' alt="Icon advanced" />
+                        <div className="card-body p-0 ml-3 ml-md-0">
+                            <h6 className="card-title m-0 font-tertiary-color">Advanced</h6>
+                            <p className="card-text font-primary-color">$12/mo</p>
+                        </div>
+                    </label>
+                    <input
+                        name='plan'
+                        id='pro'
+                        type="radio"
+                        className='d-none'
+                        value="pro"
+                        onChange={(e) => setPlan(e.target.value)}
+                    />
+                    <label htmlFor="pro" className="card flex-row p-3 border-custom flex-md-column text-left width-custom">
+                        <img src={iconPro} className='mb-md-5' width='42' alt="Icon pro" />
+                        <div className="card-body p-0 ml-3 ml-md-0">
+                            <h6 className="card-title m-0 font-tertiary-color">Pro</h6>
+                            <p className="card-text font-primary-color">$14/mo</p>
+                        </div>
+                    </label>
+                </div>
                 <div className='bg-modality-color d-flex justify-content-around align-items-center border-custom p-3 my-md-2'>
                     <h6 className='m-0 font-tertiary-color'>Monthly</h6>
                     <div className='bg-button-color icon'>
--- src/pages/your-info/index.jsx
+++ src/pages/your-info/index.jsx
@@ -1,24 +1,30 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
 import './index.scss'
 
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { object, string } from 'yup';
+
+const schema = object ({
+    name: string().required("This field is required"),
+    email: string().required("This field is required"),
+    phone: string().required("This field is required"),
+})
+
 function YourInfo() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
+    const {
+        register,
+        handleSubmit: onSubmit,
+        watch,
+        formState: { errors },
+    } = useForm({resolver: yupResolver(schema)})
 
     const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-
+    const handleSubmit = () => {
         navigate("/select-plan")
     }
-
-    console.log(name)
-    console.log(email)
-    console.log(phone)
 
     return (
         <section className="bg-white d-flex flex-column justify-content-between" id="your-info">
@@ -26,39 +32,41 @@ function YourInfo() {
                 <h3 className='font-tertiary-color'>Personal info</h3>
                 <p className='font-primary-color'>Please provide your name, email address, and phone number.</p>
             </div>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="name" className='font-tertiary-color m-0 mt-3'>Name</label>
-                <input className='w-100 p-1 px-2'
-                    id='name'
-                    type="text"
-                    placeholder='e.g. Stephen King'
-                    required
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
-                <label htmlFor="email" className='font-tertiary-color m-0 mt-3'>Email Address</label>
-                <input className='w-100 p-1 px-2'
-                    id='email'
-                    type="email"
-                    placeholder='e.g. stephenking@lorem.com'
-                    required
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <label htmlFor="phone" className='font-tertiary-color m-0 mt-3'>Phone Number</label>
-                <input className='w-100 p-1 px-2'
-                    id='phone'
-                    type="number"
-                    placeholder='e.g. +1 234 567 890'
-                    required
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                />
+            <form className='mt-3' onSubmit={onSubmit(handleSubmit)}>
+                <div className='d-flex flex-wrap justify-content-between align-items-center py-2'>
+                    <label htmlFor="name" className='font-tertiary-color m-0'>Name</label>
+                    <span className='text-danger font-weight-bold error'>{errors?.name?.message}</span>
+                    <input className={errors?.name?.message ? 'w-100 p-1 px-2 border border-danger rounded' : 'w-100 border rounded p-1 px-2'}
+                        id='name'
+                        type='text'
+                        placeholder='e.g. Stephen King'
+                        {...register('name')}
+                    />
+                </div>
+                <div className='d-flex flex-wrap justify-content-between align-items-center py-2'>
+                    <label htmlFor="email" className='font-tertiary-color m-0'>Email Address</label>
+                    <span className='text-danger font-weight-bold error'>{errors?.email?.message}</span>
+                    <input className={errors?.email?.message ? 'w-100 p-1 px-2 border border-danger rounded' : 'w-100 border rounded p-1 px-2'}
+                        id='email'
+                        type='email'
+                        placeholder='e.g. stephenking@lorem.com'
+                        {...register('email')}
+                    />
+                </div>
+                <div className='d-flex flex-wrap justify-content-between align-items-center py-2'>
+                    <label htmlFor="phone" className='font-tertiary-color m-0'>Phone Number</label>
+                    <span className='text-danger font-weight-bold error'>{errors?.phone?.message}</span>
+                    <input className={errors?.phone?.message ? 'w-100 p-1 px-2 border border-danger rounded' : 'w-100 border rounded p-1 px-2'}
+                        id='phone'
+                        type='text'
+                        placeholder='e.g. +1 234 567 890'
+                        {...register('phone')}
+                    />
+                </div>
                 <div className="d-flex justify-content-end p-3 px-md-0 position-custom">
                     <button type='submit' className='border-0 rounded p-2 px-md-4 bg-button-color text-white weight-custom'>Next Step</button>
                 </div>
             </form>
-
         </section>
     )
 }
